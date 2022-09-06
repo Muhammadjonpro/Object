1d39d98b3ead2e4a4999a6d1b8403916da5b6ecf
Muhammadjonpro <rahmatullayevm711@gmail.com>
2022-09-06 21:34:53 +0500

    object

---
+++ main.js
@@ -0,0 +1,78 @@
+//primimtive //non primetive
+
+// let number = 1;
+// let number2 = number;
+// number2 = 7;
+// console.log(`the first value is ${number}`);
+// console.log(`the second value is ${number2}`);
+
+// let person = {name:'bob'};
+// let person2 = person;
+// person2.name = 'susy';
+// console.log(`the name of the first person is ${person.name}`);
+// console.log(`the name of the second person is ${person2.name}`);
+
+//object
+
+//  let person = {
+//     firstName: 'John',
+//     lastName: 'Doe'
+// };
+
+// console.log(person.firstName);
+// console.log(person.lastName);
+
+
+
+
+// let person = {
+//     age: 25,
+//     firstName: 'John',
+//     lastName: 'Doe'
+// };
+
+// console.log(person['firstName']);
+// console.log(person['lastName']);
+
+
+// let address = {
+//     buildingNo:3960, 
+//     // building no: 3960, 
+//     street: 'North 1st street',
+//     state: 'CA',
+//     country: 'USA'
+// };
+// console.log(address.buildingNo);
+
+
+// let person = {
+//     firstName: 'John',
+//     lastName: 'Doe'
+// };
+
+// person.firstName = 'Jane'; 
+
+// console.log(person);
+
+
+
+// let person = {
+//     age: 25,
+//     firstName: 'John',
+//     lastName: 'Doe'
+// };
+
+// console.log(delete person['firstName']); 
+// console.log(person)
+
+
+
+// let employee = {
+//     firstName: 'Peter',
+//     lastName: 'Doe',
+//     employeeId: 1
+// };
+
+// console.log('ssn' in employee); 
+// console.log('employeeId' in employee);
+
